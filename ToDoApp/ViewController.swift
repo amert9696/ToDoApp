8288dfe5b7f4e7656dfc6abdbfedcbc2ae9efafe
AMG <alimertgulec38@gmail.com>
2022-02-23 14:34:27 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Ali Mert Güleç on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
